76bf63288099ed1d02c48ddb607d76d5877fcc4d
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-13 12:56:32 -0400

    Added DocumentText font weight property

--- src/Omnidoc.Core/Content/DocumentContent.cs
+++ src/Omnidoc.Core/Content/DocumentContent.cs
@@ -15,10 +15,11 @@
             Text = text;
         }
 
-        public string? Color    { get; set; }
-        public string? Font     { get; set; }
-        public double? FontSize { get; set; }
-        public string  Text     { get; }
+        public string? Color      { get; set; }
+        public string? Font       { get; set; }
+        public double? FontSize   { get; set; }
+        public int?    FontWeight { get; set; }
+        public string  Text       { get; }
     }
 
     public class DocumentImage : DocumentContent
