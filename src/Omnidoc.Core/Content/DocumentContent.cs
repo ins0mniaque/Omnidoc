d3d88f7fcbfda990740f5e1b23b1faab33e45faf
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-13 15:30:15 -0400

    Added DocumentContent Markers property

--- src/Omnidoc.Core/Content/DocumentContent.cs
+++ src/Omnidoc.Core/Content/DocumentContent.cs
@@ -2,9 +2,10 @@
 {
     public abstract class DocumentContent
     {
-        public double? Left   { get; set; }
-        public double? Top    { get; set; }
-        public double? Right  { get; set; }
-        public double? Bottom { get; set; }
+        public double?          Left    { get; set; }
+        public double?          Top     { get; set; }
+        public double?          Right   { get; set; }
+        public double?          Bottom  { get; set; }
+        public DocumentMarkers? Markers { get; set; }
     }
 }
---
+++ src/Omnidoc.Core/Content/DocumentMarkers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Omnidoc.Content
+{
+    [ Flags ]
+    public enum DocumentMarkers
+    {
+        None = 0,
+
+        StartBlock     = 1 << 0,
+        StartParagraph = 1 << 1,
+        StartLine      = 1 << 2,
+        EndLine        = 1 << 3,
+        EndParagraph   = 1 << 4,
+        EndBlock       = 1 << 5
+    }
+}
