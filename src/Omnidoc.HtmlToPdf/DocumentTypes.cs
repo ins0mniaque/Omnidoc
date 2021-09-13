2022c36742bfd6f951ffbb313ca2372bf15b5fd0
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-13 04:18:16 -0400

    Added upcoming projects

---
+++ src/Omnidoc.HtmlToPdf/DocumentTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Omnidoc.HtmlToPdf
+{
+    public static class DocumentTypes
+    {
+        static DocumentTypes ( ) => DocumentType.Register ( Html, Pdf );
+
+        public static DocumentType Html { get; } = new DocumentType ( "HyperText Markup Language", "text/html",       "html" );
+        public static DocumentType Pdf  { get; } = new DocumentType ( "Portable Document Format",  "application/pdf", "pdf"  );
+    }
+}
---
+++ src/Omnidoc.Pdf/DocumentTypes.cs
@@ -0,0 +1,9 @@
+﻿namespace Omnidoc.Pdf
+{
+    public static class DocumentTypes
+    {
+        static DocumentTypes ( ) => DocumentType.Register ( Pdf );
+
+        public static DocumentType Pdf { get; } = new DocumentType ( "Portable Document Format", "application/pdf", "pdf" );
+    }
+}
