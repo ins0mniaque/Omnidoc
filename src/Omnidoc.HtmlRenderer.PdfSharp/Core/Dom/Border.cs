ce7fcd53923b3368600700b7cb9859ebaafb6747
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-13 14:06:39 -0400

    Added HtmlRenderer.PdfSharp port

---
+++ src/Omnidoc.HtmlRenderer.PdfSharp/Core/Dom/Border.cs
@@ -0,0 +1,13 @@
+namespace Omnidoc.HtmlRenderer.Core.Dom
+{
+    /// <summary>
+    /// Border types
+    /// </summary>
+    internal enum Border
+    {
+        Top,
+        Right,
+        Bottom,
+        Left
+    }
+}
