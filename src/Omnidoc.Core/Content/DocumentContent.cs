edac8c7c06cf1651482f818ade2fd03cc2196b73
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-13 03:23:47 -0400

    Added DocumentText font and color properties

--- src/Omnidoc.Core/Content/DocumentContent.cs
+++ src/Omnidoc.Core/Content/DocumentContent.cs
@@ -10,8 +10,15 @@
 
     public class DocumentText : DocumentContent
     {
+        public DocumentText ( string text )
+        {
+            Text = text;
+        }
+
+        public string? Color    { get; set; }
+        public string? Font     { get; set; }
         public double? FontSize { get; set; }
-        public string? Text     { get; set; }
+        public string  Text     { get; }
     }
 
     public class DocumentImage : DocumentContent
