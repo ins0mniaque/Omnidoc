10c75453d410b47d34541ab2500d25e64e20f430
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-16 01:37:17 -0400

    Added Level/Levels operators

---
+++ src/Omnidoc.Core/LevelOperator.cs
@@ -0,0 +1,23 @@
+﻿namespace Omnidoc
+{
+    public static class LevelOperator
+    {
+        public static bool   Is    ( this Levels levels, Levels other ) => ( levels & other ) != 0;
+        public static Levels Match ( this Levels levels, Levels other ) => levels & other;
+        public static Level  Top   ( this Levels levels ) => levels.Is ( Levels.Document  ) ? Level.Document  :
+                                                             levels.Is ( Levels.Page      ) ? Level.Page      :
+                                                             levels.Is ( Levels.Link      ) ? Level.Link      :
+                                                             levels.Is ( Levels.Block     ) ? Level.Block     :
+                                                             levels.Is ( Levels.Table     ) ? Level.Table     :
+                                                             levels.Is ( Levels.Header    ) ? Level.Header    :
+                                                             levels.Is ( Levels.Footer    ) ? Level.Footer    :
+                                                             levels.Is ( Levels.Row       ) ? Level.Row       :
+                                                             levels.Is ( Levels.Cell      ) ? Level.Cell      :
+                                                             levels.Is ( Levels.Vertex    ) ? Level.Vertex    :
+                                                             levels.Is ( Levels.Edge      ) ? Level.Edge      :
+                                                             levels.Is ( Levels.Paragraph ) ? Level.Paragraph :
+                                                             levels.Is ( Levels.Line      ) ? Level.Line      :
+                                                             levels.Is ( Levels.Word      ) ? Level.Word      :
+                                                                                              Level.None;
+    }
+}
