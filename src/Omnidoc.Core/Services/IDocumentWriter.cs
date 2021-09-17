932755d6b758ab97833785b5aa1f971a14e93e9b
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-16 22:23:52 -0400

    Removed obsolete IDocumentWriter.Supports method

--- src/Omnidoc.Core/Services/IDocumentWriter.cs
+++ src/Omnidoc.Core/Services/IDocumentWriter.cs
@@ -1,14 +1,10 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 
-using Omnidoc.Model;
-
 namespace Omnidoc.Services
 {
     public interface IDocumentWriter : IDocumentService
     {
-        bool Supports < TContent > ( ) where TContent : Content;
-
         Task < IWritableDocument > CreateAsync  (                     CancellationToken cancellationToken = default );
         Task < IWritableDocument > PrepareAsync ( IDocument document, CancellationToken cancellationToken = default );
     }
