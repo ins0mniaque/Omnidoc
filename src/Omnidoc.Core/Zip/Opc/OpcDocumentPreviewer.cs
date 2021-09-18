1779e28bce7457a9e6e9cd6ec497aefcf164b529
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-18 12:38:13 -0400

    Added OpcDocumentPreviewer

---
+++ src/Omnidoc.Core/Zip/Opc/OpcDocumentPreviewer.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Omnidoc.Core;
+using Omnidoc.Services;
+
+namespace Omnidoc.Zip.Opc
+{
+    public class OpcDocumentPreviewer : IDocumentPreviewer
+    {
+        private static readonly IServiceDescriptor descriptor = new ServiceDescriptor
+        (
+            new [ ] { FileFormats.Opc }
+        );
+
+        public IServiceDescriptor Descriptor => descriptor;
+
+        public async Task PreviewAsync ( Stream document, Stream output, RenderingOptions options, CancellationToken cancellationToken = default )
+        {
+            using var archive = new ZipArchive ( document );
+
+            var thumbnail = await archive.TryOpenThumbnailAsync ( cancellationToken ).ConfigureAwait ( false );
+            if ( thumbnail != null )
+                await thumbnail.CopyToAsync ( output, cancellationToken ).ConfigureAwait ( false );
+        }
+    }
+}
