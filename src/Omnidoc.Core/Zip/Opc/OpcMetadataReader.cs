b2fe0afc51cecce28823b3f7f44c0f2a130311ab
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-18 12:37:47 -0400

    Added OpcMetadataReader

---
+++ src/Omnidoc.Core/Zip/Opc/OpcMetadataReader.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Omnidoc.Core;
+using Omnidoc.IO;
+using Omnidoc.Services;
+
+namespace Omnidoc.Zip.Opc
+{
+    public class OpcMetadataReader : IFileMetadataReader
+    {
+        private static readonly IServiceDescriptor descriptor = new ServiceDescriptor
+        (
+            new [ ] { FileFormats.Opc }
+        );
+
+        public IServiceDescriptor Descriptor => descriptor;
+
+        public async Task < FileMetadata? > TryReadAsync ( Stream file, CancellationToken cancellationToken = default )
+        {
+            using var archive = new ZipArchive ( file );
+
+            return await archive.TryReadMetadataAsync ( cancellationToken ).ConfigureAwait ( false );
+        }
+    }
+}
