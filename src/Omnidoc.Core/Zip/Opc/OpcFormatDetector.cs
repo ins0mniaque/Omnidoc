92c02c8c633d6ef9947d0bd144804e875a9cef8e
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-18 13:39:31 -0400

    Added OpcFormatDetector

---
+++ src/Omnidoc.Core/Zip/Opc/OpcFormatDetector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO.Compression;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Omnidoc.IO;
+using Omnidoc.Services;
+
+namespace Omnidoc.Zip.Opc
+{
+    public class OpcFormatDetector : ZipContainerFormatDetector
+    {
+        private static readonly IServiceDescriptor descriptor = new ServiceDescriptor
+        (
+            new [ ] { FileFormats.Opc }
+        );
+
+        public override IServiceDescriptor Descriptor => descriptor;
+
+        protected override Task < FileFormat? > DetectAsync ( ZipArchive archive, CancellationToken cancellationToken )
+        {
+            if ( archive is null )
+                throw new ArgumentNullException ( nameof ( archive ) );
+
+            return Task.FromResult ( archive.GetEntry ( OpcPath.Relationships ) != null ? FileFormats.Opc : null );
+        }
+    }
+}
