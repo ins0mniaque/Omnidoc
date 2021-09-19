a1e6f6e0db99a463747f68558c64f151201de9f0
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-18 21:04:42 -0400

    Added test samples support

---
+++ tests/Omnidoc.Tests/Samples/SampleAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Xunit.Sdk;
+
+namespace Omnidoc
+{
+    public sealed class SampleAttribute : DataAttribute
+    {
+        public SampleAttribute ( params string [ ] paths )
+        {
+            Paths = paths;
+        }
+
+        public IReadOnlyCollection < string > Paths { get; }
+
+        public override IEnumerable < object? [ ] > GetData ( MethodInfo testMethod )
+        {
+            var assembly = typeof(SampleAttribute).Assembly;
+            var root     = $"{ assembly.GetName ( ).Name }.Samples.";
+
+            yield return Paths.Select  ( path => assembly.GetManifestResourceStream ( root + path.Replace ( '/', '.' ) ) )
+                              .ToArray ( );
+        }
+    }
+}
---
+++ tests/Omnidoc.Tests/Samples/SamplesAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Xunit.Sdk;
+
+namespace Omnidoc
+{
+    public sealed class SamplesAttribute : DataAttribute
+    {
+        public SamplesAttribute ( string path )
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+
+        public override IEnumerable < object? [ ] > GetData ( MethodInfo testMethod )
+        {
+            var assembly = typeof ( SamplesAttribute ).Assembly;
+            var root     = $"{ assembly.GetName ( ).Name }.Samples.";
+            var path     = root + Path.Replace ( '/', '.' );
+
+            foreach ( var name in assembly.GetManifestResourceNames ( ) )
+            {
+                if ( ! name.StartsWith ( path, StringComparison.Ordinal ) )
+                    continue;
+
+                yield return new [ ] { assembly.GetManifestResourceStream ( name ) };
+            }
+        }
+    }
+}
