f3f864c4c3e088bf98bf4ae8bfa553832205cccf
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-19 19:29:37 -0400

    Added Disposable wrapper IAsyncDisposable support

--- src/Omnidoc.Core/Core/Disposables/Disposable.cs
+++ src/Omnidoc.Core/Core/Disposables/Disposable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
 
 namespace Omnidoc.Core.Disposables
 {
@@ -9,17 +10,31 @@ namespace Omnidoc.Core.Disposables
         {
             return new Disposable < T > ( instance, dispose );
         }
+
+        public static Disposable < T > AsDisposable < T > ( this T instance, Action < T > dispose, Func < T, ValueTask > disposeAsync )
+        {
+            return new Disposable < T > ( instance, dispose, disposeAsync );
+        }
     }
 
-    public sealed class Disposable < T > : IDisposable
+    public sealed class Disposable < T > : IAsyncDisposable, IDisposable
     {
-        private readonly T            instance;
-        private readonly Action < T > dispose;
+        private readonly T                     instance;
+        private readonly Action < T >          dispose;
+        private readonly Func < T, ValueTask > disposeAsync;
 
         public Disposable ( T instance, Action < T > dispose )
         {
-            this.instance = instance;
-            this.dispose  = dispose;
+            this.instance     = instance;
+            this.dispose      = dispose;
+            this.disposeAsync = DefaultDisposeAsync;
+        }
+
+        public Disposable ( T instance, Action < T > dispose, Func < T, ValueTask > disposeAsync )
+        {
+            this.instance     = instance;
+            this.dispose      = dispose;
+            this.disposeAsync = disposeAsync;
         }
 
         public T Unwrap ( ) => instance;
@@ -30,9 +45,34 @@ namespace Omnidoc.Core.Disposables
         {
             if ( ! disposed )
             {
-                dispose ( instance );
-
                 disposed = true;
+
+                dispose ( instance );
+            }
+        }
+
+        public async ValueTask DisposeAsync ( )
+        {
+            if ( ! disposed )
+            {
+                disposed = true;
+
+                await disposeAsync ( instance ).ConfigureAwait ( false );
+            }
+        }
+
+        [ SuppressMessage ( "Design", "CA1031:Do not catch general exception types", Justification = "IAsyncDisposable" ) ]
+        private ValueTask DefaultDisposeAsync ( T instance )
+        {
+            try
+            {
+                Dispose ( );
+
+                return default;
+            }
+            catch ( Exception exception )
+            {
+                return new ValueTask ( Task.FromException ( exception ) );
             }
         }
 
