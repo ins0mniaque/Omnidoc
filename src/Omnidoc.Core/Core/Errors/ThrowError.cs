9ac868ceed261a98cafe0c96b571a00640c065e5
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2021-09-18 23:48:35 -0400

    Renamed Omnidoc.Core.Internal to Omnidoc.Core.Errors

--- src/Omnidoc.Core/Core/Errors/ThrowError.cs
+++ src/Omnidoc.Core/Core/Errors/ThrowError.cs
@@ -4,13 +4,13 @@ using System.Globalization;
 
 using Omnidoc.Model;
 
-namespace Omnidoc.Core.Internal
+namespace Omnidoc.Core.Errors
 {
     public static class ThrowError
     {
         [ DoesNotReturn ] public static void ArchiveEntryNotFound  ( int     index   ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_ArchiveEntryNotFound, index ), nameof ( index ) );
-        [ DoesNotReturn ] public static void ArchiveEntryNotFound  ( string  path    ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_ArchiveEntryNotFound, path ), nameof ( path ) );
-        [ DoesNotReturn ] public static void FailedToAddElement    ( Element element ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_FailedToAddElement, element?.GetType ( ).Name ), nameof ( element ) );
+        [ DoesNotReturn ] public static void ArchiveEntryNotFound  ( string  path    ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_ArchiveEntryNotFound, path  ), nameof ( path  ) );
+        [ DoesNotReturn ] public static void FailedToAddElement    ( Element element ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_FailedToAddElement,    element?.GetType ( ).Name ), nameof ( element ) );
         [ DoesNotReturn ] public static void FailedToRemoveElement ( Element element ) => throw new ArgumentException ( string.Format ( CultureInfo.InvariantCulture, Strings.Error_FailedToRemoveElement, element?.GetType ( ).Name ), nameof ( element ) );
     }
 }
